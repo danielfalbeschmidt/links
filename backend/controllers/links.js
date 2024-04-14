c61e1ffb64222e33621bc5d7c239d4e2b39e3daf
Janne Salovaara <janne.salovaara@tuni.fi>
2024-04-14 17:28:27 +0300

    modified redirecting logic and added backend tests

--- backend/controllers/links.js
+++ backend/controllers/links.js
@@ -10,8 +10,6 @@ const linkSchema = Joi.object({
 });
 
 
-const urlMap = {};
-
 const getAllLinks = async (req, res) => {
   try {
       const response = await links.findAllLinks(); // Assuming a function findAllLinks() retrieves all items
@@ -74,13 +72,6 @@ const createLink = async (req, res) => {
           workingLink: `http://localhost:5000/${shortUrl}`
       };
 
-
-
-
-      urlMap[link.shortUrl] = link.originalUrl;
-
-      console.log("Current urlMap:", urlMap);
-
       // Create new link in the database
       const response = await links.createNewLink(link);
 
@@ -118,10 +109,14 @@ const emptyDatabase = async (req, res) => {
     }
 }
 
-const redirectToOriginalUrl = (req, res) => {
+const redirectToOriginalUrl = async (req, res) => {
     try {
       const shortUrl = req.params.shortUrl;
-      const originalUrl = urlMap[shortUrl];
+      const { originalUrl } = await links.findLinkByShortUrl(shortUrl);
+
+    //  console.log("Original URL: " + originalUrl);
+    //  console.log("Short URL: " + shortUrl);
+
       if (originalUrl) {
         res.redirect(originalUrl);
       } else {
--- backend/models/links.js
+++ backend/models/links.js
@@ -96,11 +96,40 @@ const links = {
 
         // Search for a link with the specified original URL
         const result = await collection.findOne(query);
+      //  console.log("original url result = " + JSON.stringify(result));
 
         return result;
       } catch (error) {
         console.error(error);
         throw new Error("Error finding link by original URL");
+      } finally {
+        // Close the connection
+        await client.close();
+        console.log('MongoDB connection closed');
+      }
+    },
+
+    findLinkByShortUrl: async (shortUrl) => {
+      try {
+        // Connect to the MongoDB server
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        // Access your MongoDB database and collection
+        const database = client.db('testingDb');
+        const collection = database.collection('links');
+
+        // Construct a query to search for the link by original URL
+        const query = { shortUrl: shortUrl };
+
+        // Search for a link with the specified original URL
+        const result = await collection.findOne(query);
+      //  console.log("short url result = " + JSON.stringify(result));
+
+        return result;
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error finding link by short URL");
       } finally {
         // Close the connection
         await client.close();
@@ -121,7 +150,7 @@ const links = {
         // Delete all documents from the collection
         const result = await collection.deleteMany({});
 
-        console.log(`${result.deletedCount} documents deleted`);
+      //  console.log(`${result.deletedCount} documents deleted`);
 
         return result.deletedCount; // Return the number of documents deleted
       } catch (error) {
