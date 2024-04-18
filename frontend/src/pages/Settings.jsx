65e1144c898283de5d94a4a5ccc0313ece496385
Erskari <eerosavukoski30@hotmail.com>
2024-04-18 15:25:38 +0300

    Updated settings screen

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
+import './Settings.css';
 
 const Settings = ({ onLanguageChange }) => {
   const { t, i18n } = useTranslation();
@@ -13,11 +13,20 @@ const Settings = ({ onLanguageChange }) => {
 
   return (
     <div>
-      <h1>{t('settings.title')}</h1>
-      <select value={i18n.language} onChange={handleChange}>
-        <option value="en">English</option>
-        <option value="fi">Suomi</option>
-      </select>
+      <div className="settings-container">
+        <h1>{t('settings.title')}</h1>
+        <div className="item-container">
+          <div className="item">
+          <label>
+            {t('settings.language')}:
+          </label>
+          <select className='custom-select' value={i18n.language} onChange={handleChange}>
+            <option value="en">English</option>
+            <option value="fi">Suomi</option>
+          </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
